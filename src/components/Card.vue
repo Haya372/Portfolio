9372b418a430afe6210c611182991fe0c42fa20f
Hayato Mitomi <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-09-29 18:49:14 +0900

    copy plugin

---
+++ src/components/Card.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card>
+    <v-img
+      height="256"
+      :src="src"
+      contain
+    />
+  </v-card>
+</template>
+<script>
+export default {
+  name: "Card",
+  props: {
+    image_url: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+    sub_title: {
+      type: String,
+    },
+    repo: {
+      type: String,
+    },
+    link: {
+      type: String,
+    }
+  },
+  computed: {
+    src(){
+      return this.image_url ? this.image_url : "/assets/no_image_logo.png";
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <h1>This is Index page</h1>
+    <Card />
   </div>
 </template>
 
 <script>
+import Card from '../components/Card';
 export default {
-  name: "Home"
+  name: "Home",
+  components: { Card }
 }
 </script>
